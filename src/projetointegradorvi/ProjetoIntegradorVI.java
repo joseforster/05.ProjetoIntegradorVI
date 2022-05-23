9cf43dadd3fefe15129524726cdd31eab9ce4ab3
forster <jmforster2@gmail.com>
2022-05-22 22:16:21 -0300

    Menu principal criado

--- src/projetointegradorvi/ProjetoIntegradorVI.java
+++ src/projetointegradorvi/ProjetoIntegradorVI.java
@@ -1,9 +1,11 @@
 package projetointegradorvi;
 
+import View.FrmMenuPrincipal;
+
 public class ProjetoIntegradorVI {
 
     public static void main(String[] args) {
-        
+        new FrmMenuPrincipal().setVisible(true);
     }
     
 }
